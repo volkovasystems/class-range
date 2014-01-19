137798d411e45a6ed794806d56ab17a4004e2b0d
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-01-19 18:41:30 +0800

    Added class Range.

---
+++ Range.js
@@ -0,0 +1,14 @@
+
+
+Range = function Range( startingPoint, endingPoint ){
+	/*:
+		@meta-configuration:
+			{
+				"startingPoint": "number|string|Point",
+				"endingPoint": "number|string|Point"
+			}
+		@end-meta-configuration
+	*/
+
+	
+};
